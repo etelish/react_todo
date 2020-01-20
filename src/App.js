9e72df99fb2a94f1df3c5b5734d94d96b56df90f
etelish <etelisha@googlemail.com>
2020-01-20 17:38:58 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+
+// function App()
+class App extends Component{
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+
+      },
+      {
+        id: 2,
+        title: 'Dinner with family',
+        completed: false
+
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+
+      }
+    ]
+  }
+render() {
+  console.log(this.state.todos)
+  return (
+    <div className="App">
+       <Todos />
+      <h1>App</h1>
+    </div>
+  );
+}
+}
+
+export default App;
